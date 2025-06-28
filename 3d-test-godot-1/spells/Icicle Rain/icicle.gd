229bedbb799049d8262e9a81dc8a710384315f75
ChampionX1001 <championx1001@gmail.com>
2025-06-27 21:22:45 -0400

    Add 3D icicle assets and update Icicle Rain spell
    Introduced new 3D model, texture, and material for icicles in the Icicle Rain spell. Updated icicle.gd to instantiate the new model, apply the new material, and adjust collision and physics properties. Modified main.tscn and spell_Icicle_Rain.tscn to integrate these assets and tweak related parameters.

--- 3d-test-godot-1/spells/Icicle Rain/icicle.gd
+++ 3d-test-godot-1/spells/Icicle Rain/icicle.gd
@@ -4,23 +4,26 @@ extends RigidBody3D
 
 
 
-var mesh: MeshInstance3D
+var mesh: Node3D
 var coll: CollisionShape3D
 
 func _init(x:float, y:float, z:float) -> void:
 
-	mesh = MeshInstance3D.new()
+	mesh = preload("res://spells/Icicle Rain/Icicle.glb").instantiate()
 	add_child(mesh)
-	mesh.mesh = preload("res://spells/Icicle Rain/icicleMesh.tres")
-	mesh.material_override = preload("res://spells/Icicle Rain/icicle_material.tres")
+	mesh.get_children()[0].set_surface_override_material(0, preload("res://spells/Icicle Rain/icicle_material_1.tres"))
+	#mesh = preload("res://spells/Icicle Rain/Icicle.glb")
+	#mesh.material_override = preload("res://spells/Icicle Rain/icicle_material.tres")
 	
 	coll = CollisionShape3D.new()	
-	coll.shape = preload("res://spells/Icicle Rain/icicleCollision.tres")
+	coll.shape = preload("res://spells/Icicle Rain/icicle_collision.tres")
+	coll.scale = Vector3(0.1, 0.5, 0.1)
 	add_child(coll)
 	
 	gravity_scale = 2.0
 	mass = 1
-	set_axis_velocity(Vector3(0, -10, 0))
+	set_axis_velocity(Vector3(15, -20, 0))
+	rotate_z(3.14/4)
 	
 	set_contact_monitor(true)
 	set_max_contacts_reported(1)
@@ -33,5 +36,6 @@ func _init(x:float, y:float, z:float) -> void:
 	
 func _physics_process(delta: float) -> void:
 	if get_contact_count() > 0:
-		self.queue_free()
+		#self.queue_free()
+		pass
 	
